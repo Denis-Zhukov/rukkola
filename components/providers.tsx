bdbfcea47d39277bb661a893643969d1b1d23369
Жуков Денис Владимирович <DZhukov4@alfabank.ru>
2025-11-04 06:22:43 +0300

    feat(*): add suspense

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,11 +1,16 @@
 'use client'
 
-import {ChakraProvider, defaultConfig,createSystem} from '@chakra-ui/react'
+import {ChakraProvider, defaultConfig, createSystem} from '@chakra-ui/react'
+import {Suspense} from "react";
 
 type ProvidersProps = { children: React.ReactNode }
 
 const system = createSystem(defaultConfig);
 
 export function Providers({children}: ProvidersProps) {
-    return <ChakraProvider value={system}>{children}</ChakraProvider>
+    return <ChakraProvider value={system}>
+        <Suspense fallback="Загрузка...">
+            {children}
+        </Suspense>
+    </ChakraProvider>
 }
