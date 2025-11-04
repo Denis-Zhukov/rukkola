91faaf1279af7084cfe5570e7266496f1e3cd641
Жуков Денис Владимирович <DZhukov4@alfabank.ru>
2025-11-04 06:31:21 +0300

    fix(action): add connect to db

--- app-pages/main/navbar/actions.ts
+++ app-pages/main/navbar/actions.ts
@@ -1,8 +1,10 @@
 'use server';
 
 import {Category} from "@/models/category";
+import {connectToDatabase} from "@/lib/mongoose";
 
 export const getNavItems = async () => {
+    await connectToDatabase();
     const items = await Category.find({isMenuItem: true}).sort({order: 1}).lean();
 
     return items.map(({_id, name}) => ({id: _id.toString(), name}));
